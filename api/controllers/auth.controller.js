091310193cdcc261720b40d7d0c3675a22a042bd
PMD-H002-L\Administrator <syedmuhammadzaid95@gmail.com>
2024-06-09 19:16:31 +0500

    sign up api created and error handling middleware and function created.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/errorHandler.js";
 import bcryptjs from "bcryptjs";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   const { userName, email, password } = req.body;
 
   if (
@@ -12,12 +13,12 @@ export const signUp = async (req, res) => {
     email == "" ||
     password == ""
   ) {
-    res.status(400).json({ message: "All fields are required" });
-    return;
+    next(errorHandler(400, "All fields are required"));
+    return
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  
+
   const newUser = new User({
     userName,
     email,
@@ -28,6 +29,6 @@ export const signUp = async (req, res) => {
     await newUser.save();
     res.status(200).json({ message: "User is created" });
   } catch (error) {
-    res.status(500).json({ message: error });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -1,20 +1,29 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json());
 
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(() => console.log('db is connected'))
+mongoose.connect(process.env.MONGO).then(() => console.log("db is connected"));
 
 // Routes
-app.use('/api/user' , userRouter);
-app.use('/api/auth', authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500;
+    const message = err.message || "Internal server error";
+    res.status(statusCode).json({
+      statusCode,
+      success: false,
+      message,
+    });
+  });
 
 app.listen(3000, () => {
-    console.log('server is listening to port 3000')
-})
+  console.log("server is listening to port 3000");
+});
